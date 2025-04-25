67c1ae745c4773a37832e013cfd0cec9f48569cc
franck <carnet.franck.paul@gmail.com>
2025-04-25 10:49:32 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -10,13 +10,13 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Theme Switcher Helper'] = '';
-L10n::$locales['Theme Switcher Helper widget, css and js'] = '';
-L10n::$locales['Auto'] = '';
-L10n::$locales['Light'] = '';
-L10n::$locales['Dark'] = '';
-L10n::$locales['Theme switcher helper'] = '';
-L10n::$locales['Theme switcher'] = '';
-L10n::$locales['Auto theme label:'] = '';
-L10n::$locales['Light theme label:'] = '';
-L10n::$locales['Dark theme label:'] = '';
+L10n::$locales['Theme Switcher Helper'] = 'Aide au changement de thème';
+L10n::$locales['Theme Switcher Helper widget, css and js'] = 'Widget d\'aide au changement de thème, inclus JS et CSS';
+L10n::$locales['Auto'] = 'Auto';
+L10n::$locales['Light'] = 'Clair';
+L10n::$locales['Dark'] = 'Sombre';
+L10n::$locales['Theme switcher helper'] = 'Aide au changement de thème';
+L10n::$locales['Theme switcher'] = 'Sélecteur de thème';
+L10n::$locales['Auto theme label:'] = 'Libellé de thème automatique :';
+L10n::$locales['Light theme label:'] = 'Libellé de thème clair :';
+L10n::$locales['Dark theme label:'] = 'Libellé de thème sombre :';
